d3c153de3c90d2ec6c85fca3deadff18d75f082f
Nouma <39864318+Noumaa@users.noreply.github.com>
2023-09-26 02:03:03 +0200

    Enchanting half done

--- src/listeners/EnchantsListener.php
+++ src/listeners/EnchantsListener.php
@@ -9,10 +9,12 @@ use pocketmine\block\VanillaBlocks;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\item\enchantment\Enchantment;
+use pocketmine\item\enchantment\EnchantmentInstance;
 use pocketmine\item\enchantment\SharpnessEnchantment;
 use pocketmine\item\enchantment\VanillaEnchantments;
 use pocketmine\item\Item;
 use pocketmine\item\Sword;
+use pocketmine\player\Player;
 
 class EnchantsListener implements Listener
 {
@@ -20,10 +22,30 @@ class EnchantsListener implements Listener
     public function onRightClickOnEnchantingTable(PlayerInteractEvent $event) {
         $player = $event->getPlayer();
         $block = $event->getBlock();
-        $item = $player->getInventory()->getItemInHand();
 
         if ($block->getTypeId() != VanillaBlocks::ENCHANTING_TABLE()->getTypeId()) return;
         $event->cancel();
+
+        self::openEnchantingForm($player);
+    }
+
+    private static function enchant(Player $player, EnchantmentInstance $enchantment, int $cost) {
+        if ($player->getXpManager()->getXpLevel() < $cost) {
+            $difference = $cost - $player->getXpManager()->getXpLevel();
+            $player->sendMessage("§cVous n'avez pas les niveaux requis ! §7Il vous manque §e$difference niveau(x)§7.");
+            return;
+        }
+
+        $player->getXpManager()->subtractXpLevels($cost);
+
+        $item = $player->getInventory()->getItemInHand()->addEnchantment($enchantment);
+        $player->getInventory()->setItemInHand($item);
+
+        self::openEnchantingForm($player);
+    }
+
+    private static function openEnchantingForm(Player $player) {
+        $item = $player->getInventory()->getItemInHand();
 
         $buttons = [new CloseButton("§cFermer")];
 
@@ -31,8 +53,8 @@ class EnchantsListener implements Listener
             $sharpness = $item->getEnchantmentLevel(VanillaEnchantments::SHARPNESS()) + 1;
             $unbreaking = $item->getEnchantmentLevel(VanillaEnchantments::UNBREAKING()) + 1;
             $fire_aspect = $item->getEnchantmentLevel(VanillaEnchantments::FIRE_ASPECT()) + 1;
-            
-            $buttons[] = new Button("§eTranchant $sharpness §r- §e" . $sharpness * 8 . " niveaux");
+
+            $buttons[] = new Button("§eTranchant $sharpness §r- §e" . $sharpness * 8 . " niveaux", fn (Player $player, Button $selected) => self::enchant($player, new EnchantmentInstance(VanillaEnchantments::SHARPNESS(), $sharpness), $sharpness * 8));
             $buttons[] = new Button("§eSolidité $unbreaking §r- §e" . $unbreaking * 8 . " niveaux");
             $buttons[] = new Button("§eAura de feu $fire_aspect §r- §e" . $fire_aspect * 8 . " niveaux");
         } else {
